ee6018c8d3815c49b01ea3cec02d03aaca882ed6
Nathan Molby <nathan.molby@willowtreeapps.com>
2024-05-17 10:00:33 -0400

    Initial Commit

---
+++ TestingGeometryReader/TestingGeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGeometryReaderApp.swift
+//  TestingGeometryReader
+//
+//  Created by Nathan Molby on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingGeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
